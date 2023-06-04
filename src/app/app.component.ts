df5e148c5e54968ef65629135a98408045e1a705
Muhammed Mehti GÜLER <mehti.guler@gmail.com>
2023-06-04 18:00:10 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AngularTodo';
+  isUpdateFormctive:boolean=false;
+  work:string="";
+  updateWork:string="";
+  works:string[]=[];
+  index:number=0;
+save(){
+  this.works.push(this.work);
+  this.work="";
+}
+remove(index:number){
+  let result:boolean=confirm("Kaydı Silmek İstiyormusunuz?");
+  if (result){
+    this.works.splice(index,1);
+  }
+}
+get(work:string,index:number){
+  this.updateWork=work;
+  this.index=index;
+  this.isUpdateFormctive=true;
+}
+update(){
+    this.works[this.index]=this.updateWork;
+    this.cancel();
+}
+cancel(){
+  this.isUpdateFormctive=false;
+}
+changeInputClass(){
+  if(this.work.length<3)
+    return "form-control is-invalid"
+    return "form-control is-valid"
+}
+
 }
